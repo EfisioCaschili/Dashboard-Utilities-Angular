d5f4dc3b17ab3aac851a350377ca18fde39bf2e7
EfisioCaschili <118173025+EfisioCaschili@users.noreply.github.com>
2025-12-26 09:40:49 +0100

    New feature on frontend added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit, output } from '@angular/core';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+//import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent   {
 
   output: any = null;
-
+  chart: any;
   constructor(private http: HttpClient) {}
   
   ngOnInit(): void {
+    interface StatusRow {
+      [key: string]: any;
+    }
+    interface StatusResponse {
+      result: StatusRow[];
+    }
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = {}
-    this.http.post('http://127.0.0.1:5000/api/sim_dashboard/status', body, { headers })
+    this.http.post<StatusResponse>('http://127.0.0.1:5000/api/sim_dashboard/status', body, { headers })
       .subscribe({
         next: (response) => {
-          this.output = response;
+          this.output = response.result;
+          
           console.log('Home data:', this.output);
         },
         error: (err) => {
-          console.error('Errore caricamento dati Home:', err);
+          console.error('Data loading error:', err);
         }
       });
 }
